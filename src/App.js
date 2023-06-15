325528b578dbaafe4f22cfbae3e06f9dadb788eb
AishaKeita <aisha.keita@ingka.com>
2023-06-15 17:09:38 +0200

    Exploring react props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Heading from './Heading'
 
 function Header() {
   return <h1>Concept of react</h1>
 }
 
+
 function App() {
-  return <Header />;
+  return (
+  <>
+  <Header />
+  <Heading firstName="Shatou" lastName="Njie"/>
+  </>
+  );
 }
 
 export default App;
